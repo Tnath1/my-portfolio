9fd124a74462bea6f2f21be1e5758056bae9da6e
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-08-23 17:44:20 +0100

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-blue-400 selection:text-blue-950">
+      <div className="flixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+      <div className=" container mx-auto px-8">
+        <Navbar />
+      </div>
+    </div>
   );
 }
 
