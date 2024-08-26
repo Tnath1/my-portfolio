6a858954a13bbb4c42f1cf801efebf51286c09f8
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-08-26 12:41:03 +0100

    debbuged some issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import Hero from "./components/Hero";
 import Marquee from "./components/Marquee";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 // import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-blue-400 selection:text-blue-950 bg-black pb-8">
-      <div className="relative bg-black-100 flex justify-center items-center flex-col h-[50rem] mx-auto sm:px-10 px-5">
-        <div className="max-w-7xl w-full">
+    <div className=" text-neutral-300 antialiased selection:bg-blue-400 selection:text-blue-950 ">
+      <div className="relative  bg-black-100 flex justify-center items-center overflow-hidden flex-col h-[50rem] mx-auto sm:px-10 px-5">
+        <div className=" max-w-7xl w-full ">
           <Hero />
         </div>
       </div>
       <div className="flixed top-0 -z-10 h-full w-full">
-        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        <div className="absolute top-0 z-[-2] h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
-      <div className=" container mx-auto px-8 bg-black">
-      <hr className="border-t-4 border-dashed border-gray-400 " />
+      <div className="  px-8 bg-black">
+        <hr className="border-t-4 border-dashed border-gray-400 " />
 
-        
         {/* <Navbar /> */}
         <Marquee />
         <hr className="border-t-4 border-dashed border-gray-400 " />
-
+        <Skills />
+        {/* <hr className="border-t-4 border-dashed border-gray-400 " /> */}
+        
+        <Projects />
       </div>
     </div>
   );
